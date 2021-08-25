1e60ff9f7ed2ef1372ab260797160f1768d15544
Devendra <dechouhan@gmail.com>
2021-08-25 20:33:31 +0530

    Add live city weather and search city weather functionality

--- src/routers/weatherRouter.js
+++ src/routers/weatherRouter.js
@@ -1,22 +1,29 @@
 const axios = require("axios");
 const express = require("express");
 const routerWeather = new express.Router();
-const https = require("https");
 const History = require("../models/historyModel");
+
 routerWeather.get("/showweather", async (req, res) => {
-      try {
-        const response = await axios.get(
-            `https://api.openweathermap.org/data/2.5/weather?q=${req.query.cityName}&APPID=675c51d119efd53840c834467fc005a4`
-        );
-        //console.log(response.data);
-        // const addSearchCityHistory =await new History({
-        //     email:req.query.email
-        // })
-        // addSearchCityHistory.save()
-        res.send(response.data)
-      } catch (error) {
-        console.log(error.response.data);
-      }
-    })
+  try {
+    const response = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${req.query.cityName}&APPID=675c51d119efd53840c834467fc005a4`
+    );
+    res.send(response.data);
+  } catch (error) {
+    console.log(error.response.data);
+  }
+});
+
+routerWeather.get("/showweatherbycordinate", async (req, res) => {
+  try {
+    const { latitude, longitude } = req.query;
+    const response = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&exclude={part}&appid=675c51d119efd53840c834467fc005a4`
+    );
+    res.send(response.data);
+  } catch (error) {
+    console.log(error.response.data);
+  }
+});
 
 module.exports = routerWeather;
